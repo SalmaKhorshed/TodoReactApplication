ce7eff6ad1967cbf07bb692f234ae3a92667cb5c
SalmaKhorshed <eng.salmaahmed6@gmail.com>
2025-01-09 21:12:25 +0200

    Removed splash color on clicking on navbar

--- src/navigation/BottomTabs.tsx
+++ src/navigation/BottomTabs.tsx
@@ -5,7 +5,7 @@ import HomeScreen from '../screens/HomeScreen/Home';
 import Settings from '../screens/SettingsScreen/Settings'
 import Category from '../screens/CategoryScreen/Category';
 import Profile from '../screens/ProfileScreen/Profile';
-import { Text, View } from 'react-native';
+import { Text, TouchableWithoutFeedback, View } from 'react-native';
 import { House , Shapes,CircleUserRound,SettingsIcon} from 'lucide-react-native';
 
 
@@ -27,33 +27,53 @@ const BottomTabs= () => {
         },
       }}
     >
-         <Tab.Screen name="Home" component={HomeScreen} options={{
+         <Tab.Screen name="Home" component={HomeScreen}  options={{
+           tabBarButton: (props) => (
+            <TouchableWithoutFeedback onPress={props.onPress}>
+              <View style={{marginLeft: 30}}>{props.children}</View>
+            </TouchableWithoutFeedback>
+          ),
             tabBarIcon: () => (
-              <View style={{ alignItems: 'center', justifyContent: 'center',top: 20 , width: 200  }}>
+              <View style={{ alignItems: 'center', justifyContent: 'center',top: 30 , width: 200  }}>
                <House size={20} color='#fff' />
                 <Text style={{ color:  '#fff',paddingTop: 3}}>Home</Text>
               </View>
             ),
          }}/>
          <Tab.Screen name="Category" component={Category} options={{
+            tabBarButton: (props) => (
+                <TouchableWithoutFeedback onPress={props.onPress}>
+                  <View style={{marginLeft: 30}} >{props.children}</View>
+                </TouchableWithoutFeedback>
+              ),
             tabBarIcon: () => (
-              <View style={{ alignItems: 'center', justifyContent: 'center',top: 20 , width:200  }}>
+              <View style={{ alignItems: 'center', justifyContent: 'center',top: 30 , width:200  }}>
                <Shapes size={20} color='#fff' />
                 <Text style={{ color:  '#fff',paddingTop: 3}}>Category</Text>
               </View>
             ),
          }} />
          <Tab.Screen name="Profile" component={Profile} options={{
+            tabBarButton: (props) => (
+                <TouchableWithoutFeedback onPress={props.onPress}>
+                  <View style={{marginLeft: 30}}>{props.children}</View>
+                </TouchableWithoutFeedback>
+              ),
             tabBarIcon: () => (
-              <View style={{ alignItems: 'center', justifyContent: 'center',top: 20 , width:200  }}>
+              <View style={{ alignItems: 'center', justifyContent: 'center',top: 30 , width:200  }}>
                <CircleUserRound size={20} color='#fff' />
                 <Text style={{ color:  '#fff',paddingTop: 3}}>Profile</Text>
               </View>
             ),
          }} />
          <Tab.Screen name="Settings" component={Settings} options={{
+            tabBarButton: (props) => (
+                <TouchableWithoutFeedback onPress={props.onPress}>
+                  <View style={{marginLeft: 30}}>{props.children}</View>
+                </TouchableWithoutFeedback>
+              ),
             tabBarIcon: () => (
-              <View style={{ alignItems: 'center', justifyContent: 'center',top: 20 , width:200  }}>
+              <View style={{ alignItems: 'center', justifyContent: 'center',top: 30 , width:200  }}>
                <SettingsIcon size={20} color='#fff' />
                 <Text style={{ color:  '#fff',paddingTop: 3}}>Settings</Text>
               </View>
