7428bdef23c2826dbc62b460cba28f2f2f7421d0
SalmaKhorshed <eng.salmaahmed6@gmail.com>
2025-01-09 18:08:57 +0200

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -21,7 +21,6 @@ import {
   Colors,
   DebugInstructions,
   Header,
-  LearnMoreLinks,
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
 
@@ -76,7 +75,7 @@ function App(): React.JSX.Element {
           style={{
             backgroundColor: isDarkMode ? Colors.black : Colors.white,
           }}>
-          <Section title="Step One">
+          <Section title="Step One again">
             Edit <Text style={styles.highlight}>App.tsx</Text> to change this
             screen and then come back to see your edits.
           </Section>
@@ -89,7 +88,7 @@ function App(): React.JSX.Element {
           <Section title="Learn More">
             Read the docs to discover what to do next:
           </Section>
-          <LearnMoreLinks />
+         
         </View>
       </ScrollView>
     </SafeAreaView>
