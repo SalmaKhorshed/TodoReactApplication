5618554785877542ad62f659bb0fd2a97e740259
SalmaKhorshed <eng.salmaahmed6@gmail.com>
2025-01-10 16:31:52 +0200

    Added stylesheet to bottom navigation

--- src/navigation/BottomTabs.tsx
+++ src/navigation/BottomTabs.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/no-unstable-nested-components */
 import React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
@@ -6,21 +7,83 @@ import HomeScreen from '../screens/HomeScreen/Home';
 
 import Category from '../screens/CategoryScreen/Category';
 import Profile from '../screens/ProfileScreen/Profile';
-import { Text, TouchableWithoutFeedback, View } from 'react-native';
+import { StyleSheet, Text, TouchableWithoutFeedback, View } from 'react-native';
 import { House , Shapes,CircleUserRound,SettingsIcon} from 'lucide-react-native';
 import Settings from '../screens/SettingsScreen/Settings';
 
 const Tab = createBottomTabNavigator();
 
 
-const BottomTabs= () => {
-    
+const BottomTabs = () => {
    return (
     <Tab.Navigator
       screenOptions={{
         tabBarShowLabel: false,
-        tabBarStyle: {
-          position: 'absolute',
+        tabBarStyle: styles.tabBar,
+      }}
+    >
+         <Tab.Screen name="Home" component={HomeScreen}  options={{
+           tabBarButton: (props) => (
+            <TouchableWithoutFeedback onPress={props.onPress}>
+              <View style={styles.view}>{props.children}</View>
+            </TouchableWithoutFeedback>
+          ),
+            tabBarIcon: () => (
+              <View style={styles.tabBarView}>
+               <House size={20} color="#fff" />
+                <Text style={styles.tabText}>Home</Text>
+              </View>
+            ),
+         }}/>
+         <Tab.Screen name="Category" component={Category} options={{
+            tabBarButton: (props) => (
+                <TouchableWithoutFeedback onPress={props.onPress}>
+                  <View style={styles.view} >{props.children}</View>
+                </TouchableWithoutFeedback>
+              ),
+            tabBarIcon: () => (
+              <View style={styles.tabBarView}>
+               <Shapes size={20} color="#fff" />
+                <Text style={styles.tabText}>Category</Text>
+              </View>
+            ),
+         }} />
+         <Tab.Screen name="Profile" component={Profile} options={{
+            tabBarButton: (props) => (
+                <TouchableWithoutFeedback onPress={props.onPress}>
+                  <View style={styles.view}>{props.children}</View>
+                </TouchableWithoutFeedback>
+              ),
+            tabBarIcon: () => (
+              <View style={styles.tabBarView}>
+               <CircleUserRound size={20} color="#fff" />
+                <Text style={styles.tabText}>Profile</Text>
+              </View>
+            ),
+         }} />
+         <Tab.Screen name="Settings" component={Settings} options={{
+            tabBarButton: (props) => (
+                <TouchableWithoutFeedback onPress={props.onPress}>
+                  <View style={styles.view}>{props.children}</View>
+                </TouchableWithoutFeedback>
+              ),
+            tabBarIcon: () => (
+              <View style={styles.tabBarView}>
+               <SettingsIcon size={20} color="#fff" />
+                <Text style={styles.tabText}>Settings</Text>
+              </View>
+            ),
+         }} />
+      </Tab.Navigator>
+   );
+};
+
+
+
+
+const styles = StyleSheet.create({
+    tabBar : {
+        position: 'absolute',
           bottom: 15,
           marginLeft: 25,
           marginRight: 25,
@@ -28,62 +91,21 @@ const BottomTabs= () => {
           borderRadius: 25,
           height: 90,
           elevation: 3,
-        },
-      }}
-    >
-         <Tab.Screen name="Home" component={HomeScreen}  options={{
-           tabBarButton: (props) => (
-            <TouchableWithoutFeedback onPress={props.onPress}>
-              <View style={{marginLeft: 30}}>{props.children}</View>
-            </TouchableWithoutFeedback>
-          ),
-            tabBarIcon: () => (
-              <View style={{ alignItems: 'center', justifyContent: 'center',top: 30 , width: 200  }}>
-               <House size={20} color='#fff' />
-                <Text style={{ color:  '#fff',paddingTop: 3}}>Home</Text>
-              </View>
-            ),
-         }}/>
-         <Tab.Screen name="Category" component={Category} options={{
-            tabBarButton: (props) => (
-                <TouchableWithoutFeedback onPress={props.onPress}>
-                  <View style={{marginLeft: 30}} >{props.children}</View>
-                </TouchableWithoutFeedback>
-              ),
-            tabBarIcon: () => (
-              <View style={{ alignItems: 'center', justifyContent: 'center',top: 30 , width:200  }}>
-               <Shapes size={20} color='#fff' />
-                <Text style={{ color:  '#fff',paddingTop: 3}}>Category</Text>
-              </View>
-            ),
-         }} />
-         <Tab.Screen name="Profile" component={Profile} options={{
-            tabBarButton: (props) => (
-                <TouchableWithoutFeedback onPress={props.onPress}>
-                  <View style={{marginLeft: 30}}>{props.children}</View>
-                </TouchableWithoutFeedback>
-              ),
-            tabBarIcon: () => (
-              <View style={{ alignItems: 'center', justifyContent: 'center',top: 30 , width:200  }}>
-               <CircleUserRound size={20} color='#fff' />
-                <Text style={{ color:  '#fff',paddingTop: 3}}>Profile</Text>
-              </View>
-            ),
-         }} />
-         <Tab.Screen name="Settings" component={Settings} options={{
-            tabBarButton: (props) => (
-                <TouchableWithoutFeedback onPress={props.onPress}>
-                  <View style={{marginLeft: 30}}>{props.children}</View>
-                </TouchableWithoutFeedback>
-              ),
-            tabBarIcon: () => (
-              <View style={{ alignItems: 'center', justifyContent: 'center',top: 30 , width:200  }}>
-               <SettingsIcon size={20} color='#fff' />
-                <Text style={{ color:  '#fff',paddingTop: 3}}>Settings</Text>
-              </View>
-            ),
-         }} />
-      </Tab.Navigator>
-   );
-}
+    },
+    view:{
+        marginLeft: 30,
+    },
+    tabBarView : {
+        alignItems: 'center',
+        justifyContent: 'center',
+        top: 30 ,
+        width: 200,
+    },
+    tabText : {
+        color:  '#fff',
+        paddingTop: 3,
+    },
+
+});
+
 export default BottomTabs;
